8acc0a445b448cb60ea490bd08b0e9fac5e9e254
Daniela Diaz Bejarano <60675531+DanielaDiazBej@users.noreply.github.com>
2021-04-30 16:17:20 -0500

    Cm2
    Botón de Tower y Settings funcionando
    inserta la torre falta poder ubicarla

---
+++ Assets/Scripts/UI/BtTowers.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtTowers : MonoBehaviour
+{
+    public GameObject Tower1;
+    public GameObject Tower2;
+    public GameObject Tower3;
+    public Button BtTower1;
+    public Button BtTower2;
+    public Button BtTower3;
+
+    void Start()
+    {
+        Button BtnTower1 = BtTower1.GetComponent<Button>();
+        Button BtnTower2 = BtTower2.GetComponent<Button>();
+        Button BtnTower3 = BtTower3.GetComponent<Button>();
+        BtnTower1.onClick.AddListener(BtTower1OnClick);
+        BtnTower2.onClick.AddListener(BtTower2OnClick);
+        BtnTower3.onClick.AddListener(BtTower3OnClick);
+    }
+
+    void BtTower1OnClick()
+    {
+        Tower1.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        Instantiate(Tower1);
+
+        Debug.Log("Torre1");
+
+
+    }
+    void BtTower2OnClick()
+    {
+        Tower2.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        Instantiate(Tower2);
+        Debug.Log("Torre2");
+    }
+    void BtTower3OnClick()
+    {
+        Tower3.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+        Instantiate(Tower3);
+        Debug.Log("Torre3");
+    }
+}
---
+++ Assets/Scripts/UI/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public Button BtAntenna;
+    public Button BtPhysicalP;
+    public Button BtShowCover;
+
+    void Update()
+    {
+        BtAntenna.interactable = false;
+        BtPhysicalP.interactable = false;
+        BtShowCover.interactable = false;
+    }
+
+}
